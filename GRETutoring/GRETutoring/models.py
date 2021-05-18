bc3111c9322597be99af51d3baad558ea0db5310
Mystery3434 <30279275+Mystery3434@users.noreply.github.com>
2021-05-18 12:28:31 +0800

    Free SLots added for tutors. Only DB, not UI.

--- GRETutoring/GRETutoring/models.py
+++ GRETutoring/GRETutoring/models.py
@@ -3,45 +3,61 @@ from GRETutoring import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
-def load_student(student_id):
-    return Student.query.get(int(student_id))
+def load_user(id):
+    return User.query.get(int(id))
 
-class Student(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email=db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.String(20), nullable=False, default = "Student")
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #nullable=False, since they have to have at least the default
     password = db.Column(db.String(60), nullable=False)
-    events = db.relationship('Event', backref='student', lazy=True)
+    student_classes = db.relationship('Event', backref='student', lazy=True, foreign_keys='Event.student_id')
+    tutor_classes = db.relationship('Event', backref='tutor', lazy=True, foreign_keys='Event.tutor_id')
+    tutor_free_slots = db.relationship('FreeSlot', backref='tutor', lazy=True)
 
     def __repr__(self):
-        return f"Student('{self.username}', '{self.email}','{self.image_file})"
+        return f"{self.role}('{self.username}', '{self.email}','{self.image_file})"
 
-class Tutor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email=db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #nullable=False, since they have to have at least the default
-    password = db.Column(db.String(60), nullable=False)
-    events = db.relationship('Event', backref='tutor', lazy=True)
-    free_slots = db.relationship('FreeSlot', backref='tutor', lazy=True)
-
-    def __repr__(self):
-        return f"Tutor('{self.username}', '{self.email}','{self.image_file})"
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_time = db.Column(db.DateTime, nullable=False)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-    tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    tutor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.date_time}', '{self.student_id}', '{self.tutor_id}')"
 
+
 class FreeSlot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_time = db.Column(db.DateTime, nullable=False)
-    tutor_id = db.Column(db.Integer, db.ForeignKey('tutor.id'), nullable=False)
+    tutor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.date_time}', '{self.tutor_id}')"
+
+# class Student(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email=db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #nullable=False, since they have to have at least the default
+#     password = db.Column(db.String(60), nullable=False)
+#     events = db.relationship('Event', backref='student', lazy=True)
+#
+#     def __repr__(self):
+#         return f"Student('{self.username}', '{self.email}','{self.image_file})"
+#
+# class Tutor(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email=db.Column(db.String(120), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #nullable=False, since they have to have at least the default
+#     password = db.Column(db.String(60), nullable=False)
+#     events = db.relationship('Event', backref='tutor', lazy=True)
+#     free_slots = db.relationship('FreeSlot', backref='tutor', lazy=True)
+#
+#     def __repr__(self):
+#         return f"Tutor('{self.username}', '{self.email}','{self.image_file})"
